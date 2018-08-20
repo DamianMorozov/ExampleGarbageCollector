5b3e3bbb8f5d5ce114a25420f41a40e3a6668400
Damian <DamianMorozov@users.noreply.github.com>
2018-08-20 17:36:32 +0300

    Example of of Garbage Collector

---
+++ ExampleGarbageCollector/ClassSome.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExampleGarbageCollector
+{
+    internal class ClassSome
+    {
+        public ClassSome()
+        {
+            MakeSomeGarbage();
+        }
+
+        // Create objects and release them to fill up memory with unused objects.
+        private void MakeSomeGarbage()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                var vt = new Version();
+            }
+        }
+    }
+}
---
+++ ExampleGarbageCollector/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace ExampleGarbageCollector
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            const uint collectionCount = 4;
+
+            // Create objects and release them to fill up memory with unused objects.
+            var somes = new Collection<ClassSome>();
+            for (int item = 0; item < collectionCount; item++)
+            {
+                somes.Add(new ClassSome());
+                Console.WriteLine(@"somes.Add(new ClassSome());");
+            }
+            Console.WriteLine();
+
+            // Determine the maximum number of generations the system GC currently supports.
+            Console.WriteLine($@"The highest generation is {GC.MaxGeneration}");
+            Console.WriteLine($@"GC.GetTotalMemory(false): {GC.GetTotalMemory(false)}");
+            Console.WriteLine();
+
+            GC.Collect();
+            Console.WriteLine($@"GC.Collect();");
+            Console.WriteLine($@"GC.GetTotalMemory(false): {GC.GetTotalMemory(false)}");
+            Console.WriteLine();
+
+            Console.Read();
+        }
+    }
+}
